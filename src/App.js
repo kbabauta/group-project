78cb871584f55ab1144c28610d6bb397cc908095
Kevin Babauta <kjr.babauta@gmail.com>
2021-02-10 15:04:49 -0700

    laying out initial framework

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Header from './Header'
+import MemeGen from './MemeGen'
+import './styles.css'
+
+function App(){
+    return (
+        <div>
+            <Header />
+            <MemeGen />
+        </div>
+    )
+}
+
+export default App
